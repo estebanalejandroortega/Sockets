7603650b98b6f0e0d05f5fefc161d5889ef3d242
estebanalejandroortega <estebanalejandroortega@hotmail.com>
2022-10-21 15:23:43 -0500

    0.1

---
+++ app.js
@@ -0,0 +1,30 @@
+const { Socket } = require('dgram')
+const express = require('express')
+const app = express()
+
+const http = require('http')
+const server = http.createServer(app)
+
+const {Server} = require('socket.io')
+const io = new Server(server)
+
+io.on('connection', (socket)=>{
+    //console.log('Un usuario se ha conectado')
+
+   /* socket.on('chat', (msg)=>{
+        console.log('Mensaje: '+msg)
+    })*/
+    socket.on('chat', (msg)=> {
+        io.emit('chat', msg)
+    })
+})
+
+app.get('/', (req, res)=>{
+    //res.send('<h1>Aplicación de CHAT</h1>')
+    //console.log(__dirname)
+    res.sendFile(`${__dirname}/cliente/index.html`)
+})
+
+server.listen(3000, ()=> {
+    console.log('servidor corriendo en http://localhost:3000')
+}) 
